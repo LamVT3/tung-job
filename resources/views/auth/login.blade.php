f804703f5f7e41f52b75ba86d3be5bbc74bc36ee
Thanh Lam <lamvt@topica.edu.vn>
2018-11-07 15:57:51 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,26 +33,39 @@
 
                         <div id="cd-login" class="is-selected">
                             <div class="page-login-form">
-                                <form role="form" class="login-form">
-                                    <div class="form-group">
+
+                                <form role="form" class="login-form" method="POST" action="{{ route('login') }}">
+                                    {{ csrf_field() }}
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                         <div class="input-icon">
                                             <i class="ti-user"></i>
-                                            <input type="text" id="sender-email" class="form-control" name="email" placeholder="Username">
+                                            <input type="text" id="sender-email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                     <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                            @endif
+
                                         </div>
                                     </div>
-                                    <div class="form-group">
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                         <div class="input-icon">
                                             <i class="ti-lock"></i>
-                                            <input type="password" class="form-control" placeholder="Password">
+                                            <input type="password" class="form-control" placeholder="Password" name="password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
-                                    <button class="btn btn-common log-btn">Login</button>
+                                    <button class="btn btn-common log-btn" type="submit">Login</button>
                                     <div class="checkbox-item">
                                         <div class="checkbox">
                                             <label for="rememberme" class="rememberme">
-                                                <input name="rememberme" id="rememberme" value="forever" type="checkbox"> Remember Me</label>
+                                                <input name="rememberme" id="rememberme" value="forever" type="checkbox" {{ old('remember') ? 'checked' : '' }}> Remember Me</label>
                                         </div>
-                                        <p class="cd-form-bottom-message"><a href="#0">Lost your password?</a></p>
+                                        <p class="cd-form-bottom-message"><a href="{{ route('password.request') }}">Lost your password?</a></p>
                                     </div>
                                 </form>
                             </div>
@@ -60,32 +73,48 @@
 
                         <div id="cd-signup">
                             <div class="page-login-form register">
-                                <form role="form" class="login-form">
-                                    <div class="form-group">
+                                <form role="form" class="login-form" method="POST" action="{{ route('register') }}">
+                                    {{ csrf_field() }}
+                                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                         <div class="input-icon">
                                             <i class="ti-user"></i>
-                                            <input type="text" id="sender-email" class="form-control" name="name" placeholder="Username">
+                                            <input type="text" id="sender-email" class="form-control" name="name" placeholder="Username" value="{{ old('name') }}" required autofocus>
+                                            @if ($errors->has('name'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
-                                    <div class="form-group">
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                         <div class="input-icon">
                                             <i class="ti-email"></i>
-                                            <input type="text" id="sender-email" class="form-control" name="email" placeholder="Email">
+                                            <input type="text" id="sender-email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}" required>
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <div class="input-icon">
+                                            <i class="ti-lock"></i>
+                                            <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="input-icon">
                                             <i class="ti-lock"></i>
-                                            <input type="password" class="form-control" placeholder="Password">
+                                            <input id="password-confirm" name="password-confirm" type="password" class="form-control" placeholder="Repeat Password" required>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <div class="input-icon">
-                                            <i class="ti-lock"></i>
-                                            <input type="password" class="form-control" placeholder="Repeat Password">
-                                        </div>
-                                    </div>
-                                    <button class="btn btn-common log-btn">Register</button>
+                                    <button type="submit" class="btn btn-common log-btn">Register</button>
                                 </form>
                             </div>
                         </div>
