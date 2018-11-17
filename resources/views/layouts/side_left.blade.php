f244c1278749e55ca029bca223cc0de1b158194f
tungtt6 <tungtt6@topica.edu.vn>
2018-11-17 14:07:41 +0700

    rm manage apply

--- resources/views/layouts/side_left.blade.php
+++ resources/views/layouts/side_left.blade.php
@@ -2,12 +2,12 @@
     <div class="right-sideabr">
         <div class="inner-box">
             <h4>Manage Account</h4>
-            <ul class="lest item">
-                <li><a href="#">Notifications <span class="notinumber">2</span></a></li>
-            </ul>
-            <ul class="lest item">
-                <li><a href="{{route('user-profile')}}">Jobs Applied</a></li>
-            </ul>
+            {{--<ul class="lest item">--}}
+                {{--<li><a href="#">Notifications <span class="notinumber">2</span></a></li>--}}
+            {{--</ul>--}}
+            {{--<ul class="lest item">--}}
+                {{--<li><a href="{{route('user-profile')}}">Jobs Applied</a></li>--}}
+            {{--</ul>--}}
             <ul class="lest">
                 <li><a href="{{route('user-show-form-reset')}}">Reset Password</a></li>
             </ul>
