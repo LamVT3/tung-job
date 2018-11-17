19037a67f635de9496bf2912bb382d15f9351dff
tungtt6 <tungtt6@topica.edu.vn>
2018-11-17 13:50:27 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+	    if(config('app.env') === 'production') {
+		    \URL::forceScheme('https');
+	    }
     }
 }
